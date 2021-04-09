393ed019a0b6bbf05a8adee38aad85af33557627
cgrzan <calvin.grzan@cuw.edu>
2021-04-09 00:09:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/csc250_spring2021_mergesort/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.csc250_spring2021_mergesort;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private EditText inputET;
+    private TextView answerTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.inputET = this.findViewById(R.id.inputET);
+        this.answerTV = this.findViewById(R.id.answerTV);
+    }
+
+    public void onFactorialButtonClicked(View v)
+    {
+        String currValue = this.inputET.getText().toString();
+        this.answerTV.setText(currValue);
+        int i = Integer.parseInt(currValue);
+
+        long factorial = multiplyNumbers(i);
+        System.out.println("Factorial of " + i + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+
+
+    }
+
+    }
+
+
+}
